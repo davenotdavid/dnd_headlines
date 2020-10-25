6b520cb21e119292a256a3e7af89472b0d863d49
Dave Park <dave.park@angieslist.com>
2020-10-25 15:25:20 -0400

    Android embedding v2 migration

--- android/app/src/main/kotlin/com/davenotdavid/dnd_headlines/MainActivity.kt
+++ android/app/src/main/kotlin/com/davenotdavid/dnd_headlines/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.davenotdavid.dnd_headlines
 
-import android.os.Bundle
-
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
+    // You do not need to override onCreate() in order to invoke
+    // GeneratedPluginRegistrant. Flutter now does that on your behalf.
+
+    // ...retain whatever custom code you had from before (if any).
 }
