cc3eca93892eb3a040fa9def5b9d7d6a946a7c3e
Dave Park <dave.park@angieslist.com>
2020-10-31 16:10:29 -0400

    Wrap unit tests in group

--- test/helper_functions_test.dart
+++ test/helper_functions_test.dart
@@ -2,15 +2,17 @@ import 'package:dnd_headlines/utils/helper_functions.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('Time difference logic has days ago', () {
-    final timeDiff = HelperFunctions.getTimeDifference('2020-01-01T00:00:00+00:00');
-    
-    expect(timeDiff.contains('days ago'), true);
-  });
+  group('Time difference logic', () {
+    test('difference has days ago', () {
+      final timeDiff = HelperFunctions.getTimeDifference('2020-01-01T00:00:00+00:00');
+      
+      expect(timeDiff.contains('days ago'), true);
+    });
 
-  test('Time difference logic does not have hours ago', () {
-    final timeDiff = HelperFunctions.getTimeDifference('2020-01-01T00:00:00+00:00');
-    
-    expect(timeDiff.contains('hours ago'), false);
+    test('difference does not have hours ago', () {
+      final timeDiff = HelperFunctions.getTimeDifference('2020-01-01T00:00:00+00:00');
+      
+      expect(timeDiff.contains('hours ago'), false);
+    });
   });
 }
